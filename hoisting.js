8f8a98e80a59ef8caf2074a88a0f67dfb315a3e0
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-06-15 22:06:44 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -10,11 +10,13 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+
+  thisIsCrazy();
+
 }
 
 function sayMyName() {
